3925fd0e1f8ebd2db54ac5a8a7ae08f0e28ba212
John Young <john.young@spreadstreet.io>
2019-02-08 19:48:31 -0500

    Making stupid mistakes

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -2,7 +2,7 @@ import jwtDecode from 'jwt-decode';
 import {SubmissionError} from 'redux-form';
 import axios from 'axios';
 
-import {API_BASE_URL} from '../config';
+import API_BASE_URL from '../config';
 import {normalizeResponseErrors} from './utils';
 import {saveAuthToken, clearAuthToken} from '../localStorage';
 
