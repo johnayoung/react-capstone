e3bf0faa2d4dcc2170f1a57b2a7bd4d916c5d725
John Young <john.young@spreadstreet.io>
2019-02-08 19:39:43 -0500

    Made changes to base url

--- client/src/config.js
+++ client/src/config.js
@@ -1,1 +1,9 @@
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api';
+let API_BASE_URL;
+
+if (process.env.NODE_ENV === 'production') {
+    API_BASE_URL = 'https://warm-peak-72707.herokuapp.com/api';
+} else {
+    API_BASE_URL = 'http://localhost:8080/api'
+}
+
+export default API_BASE_URL;
