54728ce773a025ca2399cc547fe6b117b1c999e0
John Young <john.young@spreadstreet.io>
2019-02-15 11:31:07 -0500

    Various polish tweaks and changes

--- client/src/containers/LandingPage.js
+++ client/src/containers/LandingPage.js
@@ -6,22 +6,23 @@ import { Link } from "react-router-dom";
 export default class LandingPage extends Component {
   render() {
     return (
-      <section className="h-screen w-screen flex sm:pt-8 md:pt-12 lg:pt-20 cover-background">
+      <section className="h-screen w-screen flex sm:pt-8 md:pt-12 lg:pt-20 lg:cover-background">
         <div className="container flex flex-col lg:max-w-screen-lg mx-auto px-6 py-8 sm:py-16 md:py-24">
           <div className="flex flex-col md:flex-row">
             <div className="flex flex-col mb-6 md:w-1/2">
-              <div className="flex flex-row float-left">
+              <div className="block">
                 <Icon
                   name="logo"
-                  className="fill-current w-8 h-8 text-grey-darker"
+                  className="fill-current w-8 h-8 text-grey-darker float-left"
                 />
                 <p className="text-xl sm:text-2xl md:text-3xl font-bold text-grey-darker">
                   API Hub
                 </p>
               </div>
               <div className="mb-12">
-                <h1 className="font-semibold text-grey-darkest text-4xl sm:text-5xl md:text-6xl mb-4 leadining-none">
-                  GitHub, but for APIs.
+                <h1 className="font-semibold text-grey-darkest text-4xl sm:text-5xl md:text-6xl mb-4 leading-none">
+                  GitHub, but for{" "}
+                  <span className="text-green-300">Public APIs.</span>
                 </h1>
                 <p className="text-xl sm:text-2xl text-grey-darkest leading-normal max-w-lg">
                   A (not so) Massive Online Repository for API Endpoints
